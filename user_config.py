58170805b12007508223bf7c36de5fb1d7b1bd3e
JackXhE <JackXhE@live.cn>
2024-07-30 09:27:25 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,8 +1,7 @@
 open_update = True
-source_file = "demo.txt"
-final_file = "result.txt"
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
 favorite_list = [
-    "广东珠江",
     "CCTV-1",
     "CCTV-5",
     "CCTV-5+",
@@ -10,8 +9,6 @@ favorite_list = [
     "广东体育",
     "广东卫视",
     "大湾区卫视",
-    "浙江卫视",
-    "湖南卫视",
     "翡翠台",
 ]
 open_online_search = True
@@ -22,7 +19,7 @@ open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "all"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
@@ -30,6 +27,9 @@ subscribe_urls = [
     "https://m3u.ibert.me/txt/fmml_dv6.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
     "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv6.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt"
 ]
 open_multicast = True
 region_list = ["all"]
